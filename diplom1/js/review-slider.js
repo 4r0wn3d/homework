34237e2a45c837bf8a5eca9082eee3e9bcbfacb3
Drowned <tepakot01@gmail.com>
2026-06-27 21:16:46 +0300

    Diplom 1 upload

---
+++ diplom1/js/review-slider.js
@@ -0,0 +1,20 @@
+export const reviewSlider = () => {
+    new Swiper(".reviews__slider", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 20,
+        loopedSlides: 10,
+        loopPreventsSliding: false,
+        loop: true,
+        initialSlide: 2,
+        mousewheel: {
+            forceToAxis: true,
+        },
+        navigation: {
+            prevEl: '.reviews__slider-button--prev',
+            nextEl: '.reviews__slider-button--next',
+        },
+         
+        
+    })
+};
