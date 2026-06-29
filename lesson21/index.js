986cd3fb27bbd56a0fee12f18a729eb1f550d018
Drowned <tepakot01@gmail.com>
2026-06-29 20:14:36 +0300

    hw 21 added

---
+++ lesson21/index.js
@@ -0,0 +1,18 @@
+const firstName = "Иван";
+const lastName = "Неиван";
+const isStudent = true;
+const age = 18;
+const currentYear = 2026;
+
+const birthYear = currentYear - age;
+
+console.log(
+	`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`,
+);
+let a = "123";
+let b = +"456";
+let c = Number("789");
+let d = Boolean(0);
+let e = Boolean(" ");
+let result = a + b + c + d + e;
+console.log(result);
